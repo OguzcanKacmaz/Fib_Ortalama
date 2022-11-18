1eff3d29febb7b5b2e6f45c70eb1e77e892d6e01
Mustafa Oguzcan Kacmaz <oguzcankacmaz@hotmail.com>
2022-11-18 23:07:15 +0300

    Add project files.

---
+++ Fib_Ortalama/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Ortalamasını İstediğiniz Fibonacci Dizisinin Eleman Sayısını Giriniz : ");
+int elemanSayisi=Convert.ToInt32(Console.ReadLine());
+int a = 0;
+int b = 1;
+int yedek ;
+Console.Write(a+"-");
+Console.Write(b+"-");
+for (int i = 0; i < elemanSayisi-2; i++)
+{
+    yedek = a+b;
+    a = b;
+    b=yedek;
+    Console.Write(yedek+"-");
+}
+Console.ReadKey();
